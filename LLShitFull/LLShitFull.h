66f31dbd1b28278ab30d68fc391c33c11b5009e3
Ross Willett <contact@soundspawn.com>
2015-08-30 11:16:19 -0700

    Standardize datatypes (uint16_t etc), add notes for the big feature I'm needing

--- LLShitFull/LLShitFull.h
+++ LLShitFull/LLShitFull.h
@@ -12,17 +12,20 @@ class LLSLogger:
         bool writeEvent(const __FlashStringHelper*);
         bool setLogPath(const char*);
         char* getFullCurrentLog(char*);
-        char* getLogName(char*,unsigned int);
+        char* getLogName(char*,uint16_t);
         char* getLogName(char*);
         bool setFakeRTC(uint32_t);
         bool detectMillisRollover(uint32_t);
         bool detectMillisRollover();
         uint32_t getEventTimestamp();
+        bool getRecentEventArray(uint8_t);
+        bool setAverageMessageLength(uint16_t);
 
     protected:
         char* logPath;
         uint32_t timeSync = 0;
         uint32_t lastMillisLogged = 0;
+        uint16_t avgMessageLength = 50;
 };
 
 #endif
